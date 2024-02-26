a37f050364113fd5e4c2874f72471d3bdbf959da
sofush <102423577+sofush@users.noreply.github.com>
2024-02-26 17:19:52 +0100

    feat: tilføj login til database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        String brugernavn = prompt("Indtast brugernavn:");
+        String kodeord = prompt("Indtast kodeord:");
+        final Database db = new Database(brugernavn, kodeord);
     }
 }
